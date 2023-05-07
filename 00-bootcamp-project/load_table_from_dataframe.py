0e2c1927468764d03f0c34be38402161bbf1a407
Sintop Seenoul <55233788+sintop@users.noreply.github.com>
2023-05-07 04:32:46 +0000

    refactor code

--- 00-bootcamp-project/load_table_from_dataframe.py
+++ 00-bootcamp-project/load_table_from_dataframe.py
@@ -1,13 +1,10 @@
-# Ref: https://cloud.google.com/bigquery/docs/samples/bigquery-load-table-dataframe
+# Ref: https://cloud.google.com/bigquery/docs/samples/bigquery-load-table-gcs-csv
 
 import json
 import os
-from datetime import datetime
 
-import pandas as pd
 from google.cloud import bigquery
 from google.oauth2 import service_account
-
 
 keyfile = os.environ.get("KEYFILE_PATH")
 service_account_info = json.load(open(keyfile))
@@ -18,35 +15,55 @@ client = bigquery.Client(
     credentials=credentials,
 )
 
-job_config = bigquery.LoadJobConfig(
-    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
-    autodetect=True,
-    # schema=[
-    #     bigquery.SchemaField("user_id", bigquery.SqlTypeNames.STRING),
-    #     bigquery.SchemaField("first_name", bigquery.SqlTypeNames.STRING),
-    #     bigquery.SchemaField("last_name", bigquery.SqlTypeNames.STRING),
-    #     bigquery.SchemaField("email", bigquery.SqlTypeNames.STRING),
-    #     bigquery.SchemaField("phone_number", bigquery.SqlTypeNames.STRING),
-    #     bigquery.SchemaField("created_at", bigquery.SqlTypeNames.TIMESTAMP),
-    #     bigquery.SchemaField("updated_at", bigquery.SqlTypeNames.TIMESTAMP),
-    #     bigquery.SchemaField("address_id", bigquery.SqlTypeNames.STRING),
-    # ],
-    # time_partitioning=bigquery.TimePartitioning(
-    #     type_=bigquery.TimePartitioningType.DAY,
-    #     field="created_at",
-    # ),
-    # clustering_fields=["first_name", "last_name"],
-)
+partition_list = {"events":"20210210","orders":"20210210","users":"20201023"}
+no_partition_list = ["addresses","order_items","products","promos"]
+custer_list = [[],[],["first_name", "last_name"]]
 
-for filename in os.listdir('data'):
-    print(filename)
-    file_path = "data/{}".format(filename)
-    df = pd.read_csv(file_path)
-    df.info()
+def load_data_from_file(table,clustering_fields):
+    """test"""
+    if clustering_fields:
+        job_config = bigquery.LoadJobConfig(
+            skip_leading_rows=1,
+            write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+            source_format=bigquery.SourceFormat.CSV,
+            autodetect=True,
+            time_partitioning=bigquery.TimePartitioning(
+                type_=bigquery.TimePartitioningType.DAY,
+                field="created_at",
+            ),
+            clustering_fields=clustering_fields,
+        )
+    else:
+        job_config = bigquery.LoadJobConfig(
+            skip_leading_rows=1,
+            write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+            source_format=bigquery.SourceFormat.CSV,
+            autodetect=True,
+            time_partitioning=bigquery.TimePartitioning(
+                type_=bigquery.TimePartitioningType.DAY,
+                field="created_at",
+            ),
+        )
+    file_path = f"data/{table}.csv"
+    with open(file_path, "rb") as f:
+        table_id = f"{project_id}.deb_bootcamp.{table}${partition_list[table]}"
+        job = client.load_table_from_file(f, table_id, job_config=job_config)
+        return job.result()
+def load_data_from_file_no_partition(table):
+    """test"""
+    job_config_non_partition = bigquery.LoadJobConfig(
+        skip_leading_rows=1,
+        write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+        source_format=bigquery.SourceFormat.CSV,
+        autodetect=True,
+    )
+    file_path = f"data/{table}.csv"
+    with open(file_path, "rb") as f:
+        table_id = f"{project_id}.deb_bootcamp.{table}"
+        job = client.load_table_from_file(f, table_id, job_config=job_config_non_partition)
+        return job.result()
+res = map(load_data_from_file,list(partition_list.keys()),custer_list)
+print(list(res))
 
-    table_id = "{}.deb_bootcamp.{}".format(project_id,filename.split(".")[0]) 
-    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
-    job.result()
-
-    table = client.get_table(table_id)
-    print(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+res_no_partition = map(load_data_from_file_no_partition,no_partition_list)
+print(list(res_no_partition))
